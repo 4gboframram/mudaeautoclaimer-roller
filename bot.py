602e904f5357b742fd8c60d41ed4fe52e9cf938c
4gboframram <79847791+4gboframram@users.noreply.github.com>
2021-04-05 19:33:51 -0400

    v1.2.0
    Features: 
    -Added $daily claiming to farm roll resets 
    -made $dk and $daily commands customizable in config.json
    -Fixed bug where program would try to claim character when someone views their info card if the character only has a single image
    -fixed a bunch of comments in the code again

--- bot.py
+++ bot.py
@@ -21,8 +21,11 @@ command=config["command"]
 timeBetweenRolls=config["timeBetweenRolls"]
 numberOfRolls=config["numberOfRolls"]
 typingTime=config["typingTime"]
-
+dkCommand=config["dkCommand"]
+dailyCommand=config["dailyCommand"]
 client=discord.Client() 
+
+    
 
 init() #initiate colorama for colors in the console
 def search(string):
@@ -69,22 +72,22 @@ async def dailyClaim(channel):
     print(colored(f"[{current_time}] Attempting to claim daily kakera in {chanel.name}",'magenta'))
     async with chanel.typing():
         await asyncio.sleep(typingTime)
-    await chanel.send("$dk")
+    await chanel.send(f"{dkCommand}")
     await asyncio.sleep(timeBetweenRolls)
     async for message in chanel.history(limit=5): #in case other users busy rolling, make the limit higher
-        if "Next $dk reset" in message.content and message.author.bot==True:
+        if f"Next {dkCommand} reset" in message.content and message.author.bot==True:
             try:
                 current_time = time.strftime("%D %H:%M:%S", time.localtime())
                 t=search(message.content)
                 if len(t)<=1: #basically if there's either only hours or only minutes
                                 #idk if only hours is a thing, but just in case
                     if 'h' in message.content: 
-                        print(colored(f"[{current_time}] Next $dk in '{chanel.name}' in {t[0]}h 0m. Asyncronously sleeping in '{chanel.name}' that amount of time.",'cyan'))
+                        print(colored(f"[{current_time}] Next {dkCommand} in '{chanel.name}' in {t[0]}h 0m. Asyncronously sleeping in '{chanel.name}' that amount of time.",'cyan'))
                         await asyncio.sleep(3600*t[0])
                     if 'min' in message.content: 
-                        print(colored(f"[{current_time}] Next $dk in '{chanel.name}' in 0h {t[0]}m. Asyncronously sleeping in '{chanel.name}' that amount of time.",'cyan'))
+                        print(colored(f"[{current_time}] Next {dkCommand} in '{chanel.name}' in 0h {t[0]}m. Asyncronously sleeping in '{chanel.name}' that amount of time.",'cyan'))
                         await asyncio.sleep(60*t[0])
-                print(colored(f"[{current_time}] Next $dk in '{chanel.name}' in {t[0]}h {t[1]}m. Asyncronously sleeping in '{chanel.name}' that amount of time.",'cyan'))
+                print(colored(f"[{current_time}] Next {dkCommand} in '{chanel.name}' in {t[0]}h {t[1]}m. Asyncronously sleeping in '{chanel.name}' that amount of time.",'cyan'))
                 await asyncio.sleep(3600*t[0]+60*t[1]+1) #first integer in the message is the hour, second integer is the minutes
                 await dailyClaim(channel)
             except IndexError: continue #when search() doesn't return anything, ignore the error raised and try again
@@ -98,12 +101,48 @@ async def doDailyClaim():
     for future in asyncio.as_completed(map(dailyClaim, Channels)): #basically do dk claiming asyncronously
         result = await future #because they have different wait times         
 
+async def daily(channel):
+    await asyncio.sleep(5*timeBetweenRolls-typingTime)
+    current_time = time.strftime("%D %H:%M:%S", time.localtime())
+    chanel=client.get_channel(int(channel))
+    print(colored(f"[{current_time}] Attempting to claim {dailyCommand} in {chanel.name}",'magenta'))
+    async with chanel.typing():
+        await asyncio.sleep(typingTime)
+    await chanel.send(f"{dailyCommand}")
+    await asyncio.sleep(timeBetweenRolls)
+    async for message in chanel.history(limit=5): #in case other users busy rolling, make the limit higher
+        if f"Next {dailyCommand} reset" in message.content and message.author.bot==True:
+            try:
+                current_time = time.strftime("%D %H:%M:%S", time.localtime())
+                t=search(message.content)
+                if len(t)<=1: #basically if there's either only hours or only minutes
+                                #idk if only hours is a thing, but just in case
+                    if 'h' in message.content: 
+                        print(colored(f"[{current_time}] Next {dailyCommand} in '{chanel.name}' in {t[0]}h 0m. Asyncronously sleeping in '{chanel.name}' that amount of time.",'cyan'))
+                        await asyncio.sleep(3600*t[0])
+                    if 'min' in message.content: 
+                        print(colored(f"[{current_time}] Next {dailyCommand} in '{chanel.name}' in 0h {t[0]}m. Asyncronously sleeping in '{chanel.name}' that amount of time.",'cyan'))
+                        await asyncio.sleep(60*t[0])
+                print(colored(f"[{current_time}] Next {dailyCommand} in '{chanel.name}' in {t[0]}h {t[1]}m. Asyncronously sleeping in '{chanel.name}' that amount of time.",'cyan'))
+                await asyncio.sleep(3600*t[0]+60*t[1]+1) #first integer in the message is the hour, second integer is the minutes
+                await daily(channel)
+            except IndexError: continue #when search() doesn't return anything, ignore the error raised and try again
+
+        else: #if daily claim fails due to Mudae being ratelimited or something, try again
+            await asyncio.sleep(3*timeBetweenRolls-typingTime) 
+            await daily(channel)
+    await asyncio.sleep(timeBetweenRolls-typingTime)
+   
+async def doDaily():
+    for future in asyncio.as_completed(map(daily, Channels)): #basically do daily claiming asyncronously
+        result = await future #because they have different wait times         
+
        
 @client.event
 async def on_ready(): 
     current_time = time.strftime("%D %H:%M:%S", time.localtime())
     print(colored(f'[{current_time}] Logged in as {client.user.name}','green'))
-    await asyncio.gather(doRolls(), doDailyClaim()) #when user logs on, call doRolls() and claimDaily()
+    await asyncio.gather(doRolls(), doDailyClaim(),doDaily()) #when user logs on, call doRolls(), doDailyClaim() and doDaily()
 
 @client.event
 async def on_message(message):
@@ -126,14 +165,15 @@ async def on_message(message):
                 print(colored(f"\n[{current_time}] Trying to claim: {str(title)}",'blue'))
                 
                 await asyncio.sleep(reactionDelay)
-                if message.reactions[0].emoji.id==469835876265099275: #:wleft: emoji id
-                    current_time = time.strftime("%D %H:%M:%S", time.localtime())
-                    print(colored(f'[{current_time}] Someone tried to fool the bot in channel {channelName} by sending {title}\'s info card','yellow'))
-                    return
+                if not 'React with' in message.embeds[0].description: #if a character's image card is shown, say someone tried to fool the bot
+                            current_time = time.strftime("%D %H:%M:%S", time.localtime())
+                            print(colored(f'[{current_time}] Someone tried to fool the bot in channel \'{channelName}\' by sending {title}\'s info card','yellow'))
+                            return
+                    
                 await message.add_reaction(message.reactions[0])
                 current_time = time.strftime("%D %H:%M:%S", time.localtime())
                 print(colored(f'\t[{current_time}] Reacted to {title} in channel \'{channelName}\' with ','blue')+colored(f'\'{message.reactions[0]}\'','cyan'))
-                print(colored(f"\t[{current_time}] Probably claimed {title} in channel '{channelName}'",'green')+'\n\t The above could be a false positive if the list of wanted characters has the name of another user\'s \nharem name in it, or if your claim timer is on cooldown.')
+                print(colored(f"\t[{current_time}] Probably claimed {title} in channel '{channelName}'",'green')+'\n\t The above could be a false positive if the if your claim timer is on cooldown.')
             
             except IndexError: #if no reactions
 
